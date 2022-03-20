2aa562f13a40ae63e8c109b23c8d1333b0d99a5a
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-03-20 21:34:24 +0800

    Red

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import turtle
 
 window = turtle.Screen()
-draw = turtle.Turtle()
+blue = turtle.Turtle()
 
-draw.color('#4048cd')
-draw.fillcolor('#4048cd')
-draw.begin_fill()
+blue.color('#4048cd')
+blue.fillcolor('#4048cd')
+blue.begin_fill()
 for i in range(4):
-    draw.forward(300)
-    draw.right(90)
-draw.end_fill()
+    blue.forward(300)
+    blue.right(90)
+blue.end_fill()
+
+red = turtle.Turtle()
+
+red.penup()
+red.goto(40, -40)
+red.pendown()
+red.color('#ee1d24')
+red.fillcolor('#ee1d24')
+red.begin_fill()
+for i in range(4):
+    red.forward(220)
+    red.right(90)
+red.end_fill()
 
 turtle.done()
